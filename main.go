aaf61ff222edc7c5171a9743e4b4166e047ef7a5
Ramiro Méndez López <ramirog8k@gmail.com>
2020-10-09 23:06:12 -0500

    ready to action

--- main.go
+++ main.go
@@ -3,5 +3,23 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+	// n = number of elements
+	// v = value of element to store in array
+	// t = total
+
+	// s = array(slice) to store n values
+	var n, v, t int
+	var s []int
+	
+	fmt.Scan(&n)
+
+	// bucle to get input, store it in slice, add to total
+	for i := 0; i < n; i++ {
+		fmt.Scan(&v)
+		s = append(s, v)
+		t += v
+	}
+	
+	// output is total
+	fmt.Println(t)
 }
